da347656b04226b0aff053146497430efcdc4ee9
yasir ahamed syed <sonu.yasir@gmail.com>
2013-11-12 22:43:20 +0300

    Update mars_json.py

--- mars_json.py
+++ mars_json.py
@@ -5,7 +5,7 @@ import json
 class Mars(object):
   ''' This class initializes using a file name as the arguement.
   send(json): writes json to a file
-  read(json): reads json from a file
+  read(): reads json from a file
   '''
   def __init__(self, fileName="jsonFile.json"):
     ''' This function take a file name as an arguement '''
@@ -14,6 +14,8 @@ class Mars(object):
   def send(self, data):
     ''' take a dictionary as arguement and writes it to the file
     this throws a Exception on error'''
+    if not isinstance( data, dict ):
+      raise Exception (" %r is not a dict" %( data)) 
     try :
       with io.open(self.fileName, 'w', encoding='utf-8') as f:
         f.write(unicode(json.dumps(data, ensure_ascii=False)))
@@ -22,26 +24,12 @@ class Mars(object):
         raise e
     
   def receive(self):
-    ''' receive reads the file and returns a json object
-    this throws a Exception on error'''
+    ''' receive() reads the file and returns a json object.
+    This method throws a ValueError, if the data loded is not
+    of type dictionary'''
     try :
       with open(self.fileName, 'r') as json_file:
         data = json.load(json_file)
         return data
     except Exception as e:
       raise e
-    
-
- 
-
-if __name__ == '__main__':
-  mars = Mars()
-  jsonDate={'a' : 'Hello'}
-  print mars.send(jsonDate)
-  print mars.receive()
-  
-  
-    
-
-
-       
