07333ed9aa8b191ab1cfba541e57d029af305fc1
yasir ahamed syed <sonu.yasir@gmail.com>
2013-11-12 23:44:50 +0300

    Update test_mars.py

--- test_mars.py
+++ test_mars.py
@@ -4,33 +4,30 @@ import unittest
 
 class TestMars(unittest.TestCase):
   '''
-  This is a test class to test CompanyShareAnalyzer class
+  This is a test class to test Mars class
   '''
   def setUp(self):
-    with io.open('jsonFile.json', 'w', encoding='utf-8') as f:
-        f.truncate()
+    self.mars = Mars()
 
   def testForSendAndRecieveJson(self):
     ''' This raises an exception for inconsistant data in file '''
-    mars = Mars()
+    
     jsonDate={'a' : 'Hello'}
-    mars.send(jsonDate)
-    self.assertEqual(jsonDate, mars.receive())
+    self.mars.send(jsonDate)
+    self.assertEqual(jsonDate, self.mars.receive())
     
   def testForWritingNonDictionary(self):
     ''' This raises an exception when we try to write a
     non dictionary data into a file'''
-    mars = Mars()
-    jsonDate="hello"
-    self.assertRaises(Exception, lambda: mars.send(jsonDate))
+    jsonDate="hello" 
+    self.assertRaises(Exception, lambda: self.mars.send(jsonDate))
     
   def testForReadingNonDictionary(self):
     ''' This raises an exception when we try to read a
     non dictionary data from a file'''
     with io.open('jsonFile.json', 'w', encoding='utf-8') as f:
         f.write(unicode("Some Non Dictionary data"))
-    mars = Mars()
-    self.assertRaises(Exception, lambda: mars.receive())
+    self.assertRaises(Exception, lambda: self.mars.receive())
     
 
 if __name__ == '__main__':
