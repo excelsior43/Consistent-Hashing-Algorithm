02812258248a6072550efe48dca51c0a8469f6d6
yasir ahamed syed <sonu.yasir@gmail.com>
2013-11-15 01:20:02 +0300

    Update mamcache_client.py

--- mamcache_client.py
+++ mamcache_client.py
@@ -1,4 +1,4 @@
-""" 
+"""
 When we add a new server, 1/n of the total cache keys will be lost.
 Secondly the *the best output* of this program also depends on the number
 of replications. In this scenario I used 15 replications.
@@ -6,20 +6,22 @@ of replications. In this scenario I used 15 replications.
 
 import random
 import string
-import memcache 
+import memcache
 from consistent_hash import ConsistentHash
 
 class MemcacheClient(memcache.Client):
     """ A memcache subclass. It currently allows you to add a new host at run
-    time. 
+time.
 
-    only 1/N th cache is wiped out, where N is the number of cache servers.
-    """
-    def __init__(self, *args, **kwargs):
+only 1/N th cache is wiped out, where N is the number of cache servers.
+"""
+    def __init__(self, replicas=1,*args, **kwargs):
+        self.consistent_hash=ConsistentHash(replicas=replicas)
         super(MemcacheClient, self).__init__(*args, **kwargs)
+        
 
     def _get_server(self, key):
-        """ Current implementation of Memcache client"""
+        """ Current implementation of Memcac he client"""
         for i in range(memcache.Client._SERVER_RETRIES):
           server = self.buckets[self.get_server_index(key)]
           if server.connect():
@@ -28,7 +30,7 @@ class MemcacheClient(memcache.Client):
         
     def set_servers(self, servers):
         ret=super(MemcacheClient, self).set_servers(servers)
-        self.consistent_hash=ConsistentHash(servers=self.servers)
+        self.consistent_hash.setup_servers(servers=self.servers)
         return ret
    
     def get_server_index(self,key):
@@ -37,23 +39,21 @@ class MemcacheClient(memcache.Client):
         
 
     def add_server(self, server):
-        """ Adds a host at runtime to client
-        """
+        """ Adds a host at runtime to client"""
         # Create a new host entry
         server = memcache._Host(
             server, self.debug, dead_retry=self.dead_retry,
             socket_timeout=self.socket_timeout,
             flush_on_reconnect=self.flush_on_reconnect
         )
-        # Add this to our server choices 
+        # Add this to our server choices
         self.servers.append(server)
         # Update our buckets
         self.buckets.append(server)
         self.consistent_hash.add_machine(server,len(self.servers)-1)
 
 def random_key(size):
-    """ Generates a random key
-    """
+    """ Generates a random key """
     return ''.join(random.choice(string.letters) for _ in range(size))
 
 
@@ -61,20 +61,20 @@ if __name__ == '__main__':
     # We have 7 running memcached servers
     servers = ['127.0.0.1:1121%d' % i for i in range(1,8)]
     # We have 100 keys to split across our servers
-    keys = [random_key(10) for i in range(1000)]
+    keys = [random_key(10) for i in range(10000)]
     # Init our subclass
-    client = MemcacheClient(servers=servers)
+    client = MemcacheClient(replicas=15,servers=servers)
     # Distribute the keys on our servers
     for key in keys:
         client.set(key, 1)
 
-    # Check how many keys come back 
+    # Check how many keys come back
     valid_keys = client.get_multi(keys)
     print '%s percent of keys matched' % ((len(valid_keys)/float(len(keys))) * 100)
 
     # We add another server...and pow!
     client.add_server('127.0.0.1:11218')
-    print 'Added new server' 
+    print 'Added new server'
 
     valid_keys = client.get_multi(keys)
     print '%s percent of keys stil matched' % ((len(valid_keys)/float(len(keys))) * 100)
